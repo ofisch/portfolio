0cbd5d12b814b220f82dac05e8114c4e1146ea9f
ofisch <onni.fischer@gmail.com>
2024-02-26 20:39:35 +0200

    kuvat toimii taas jee

--- my-project/src/utils/projects.js
+++ my-project/src/utils/projects.js
@@ -3,8 +3,7 @@ export const projects = [
     id: 1,
     title: "Webweave",
     description: "Tekoälyä hyödyntävä kotisivukone",
-    imageUrl:
-      "https://cdn.discordapp.com/attachments/1185220320492277900/1185220524146700449/ww.JPG?ex=658ed1a1&is=657c5ca1&hm=5214a82d1d3f76c2503e4d3c7e060ea13e180616c018366e9b81566861d9adff&",
+    imageUrl: "https://users.metropolia.fi/~onnif/portfolioKuvat/ww.JPG",
     url: "https://webweave.fi",
     githubUrl: "https://github.com/ofisch/WebWeave",
     lang: ["React", "TypeScript", "Tailwind", "Firebase"],
@@ -13,8 +12,7 @@ export const projects = [
     id: 2,
     title: "Idiomix",
     description: "Kielenopiskelusovellus",
-    imageUrl:
-      "https://cdn.discordapp.com/attachments/1185220320492277900/1185598849805463562/idiomix.JPG?ex=659031f9&is=657dbcf9&hm=e5bf0664efb7ce282deb9ed11f6d1042c6c0ceba092e0f480b5db68fb7ae6fd2&",
+    imageUrl: "https://users.metropolia.fi/~onnif/portfolioKuvat/idiomix.JPG",
     url: "https://idiomix.lm.r.appspot.com",
     githubUrl: "https://github.com/ofisch/idiomix",
     lang: ["React", "JavaScript", "Material UI"],
@@ -24,7 +22,7 @@ export const projects = [
     title: "PuuhaPlanneri",
     description: "Todo-listasovellus",
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1185220320492277900/1185600478684393523/kauppakamu.JPG?ex=6590337d&is=657dbe7d&hm=5f6c1533cc6a41ffef6769bbd10b6e0bacfcfca4e043683346cc04cd337483c0&",
+      "https://users.metropolia.fi/~onnif/portfolioKuvat/kauppakamu.JPG",
     url: "https://ostoslista-9f255.lm.r.appspot.com/landing",
     githubUrl: "https://github.com/ofisch/todoApp-firebase",
     lang: ["React", "JavaScript", "Tailwind", "Firebase"],
@@ -35,7 +33,7 @@ export const projects = [
     title: "Activout",
     description: "Aktiviteettisovellus",
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1185220320492277900/1185604974479167628/activout.JPG?ex=659037ad&is=657dc2ad&hm=be73d31ff265629a32ee75c12534078687389c2c4214454d46a87a2e47168eb7&",
+      "https://users.metropolia.fi/~onnif/portfolioKuvat/activout.JPG",
     url: "https://tictactoe-2e7c0.web.app/",
     githubUrl: "https://github.com/ofisch/activout",
     lang: ["React", "JavaScript", "Material UI"],
@@ -45,8 +43,7 @@ export const projects = [
     id: 5,
     title: "Riffsurf",
     description: "Kitarapeli",
-    imageUrl:
-      "https://cdn.discordapp.com/attachments/1185220320492277900/1185976434854989904/riffsurf.JPG?ex=659191a0&is=657f1ca0&hm=f9ef7f65a1bf4a5fbd2f2f36497795a806b0665cb2e04ce6cbf2acb00f0cffa9&",
+    imageUrl: "https://users.metropolia.fi/~onnif/portfolioKuvat/riffsurf.JPG",
     url: "https://ofisch.itch.io/riffsurf",
     githubUrl: "https://github.com/tsarmander/guitarhero2022",
     lang: ["Unity", "C#"],

59b514a5e638d1f40452d109936277de0d32dcde
ofisch <onni.fischer@gmail.com>
2024-05-15 11:39:57 +0300

    Remove unused components and styles

--- my-project/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import homeStyle from "../styles/home";
-
-export const Header = () => {
-  return (
-    <header className={homeStyle.header}>
-      <h3 className={homeStyle.h1}>onni.fi</h3>
-    </header>
-  );
-};
